56fe7a49b20f6a62769fbc2b600dd5c48da02cc4
AntonLPN <68461689+AntonLPN@users.noreply.github.com>
2021-03-03 11:22:39 +0200

    Добавьте файлы проекта.

---
+++ L_Indeksatori_25.09.2020/MyArray.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L_Indeksatori_25._09._2020
+{
+    class MyArray
+    {
+        int size_arr;
+        int[] arr;
+        int count;
+
+
+        public MyArray()
+        {
+            size_arr = 1;
+            arr = new int[size_arr];
+            count = 0;
+
+        }
+
+        public MyArray(int size_arr)
+        {
+            this.size_arr = size_arr;
+            arr = new int[this.size_arr];
+        }
+
+        //•	размер массива «length»
+        //сформировано автоматической подсказкой после написания свойства с одним геттером
+        public int Length => size_arr;
+
+        //•	количество элементов в массиве «actual»
+        public int Actual
+        {
+            get
+            {
+                int actual = 0;
+
+                for (int i = 0; i < this.arr.GetLength(0); i++)
+                {
+
+                    if (arr[i] != 0)
+                    {
+                       actual++;
+                    }
+                }
+
+                count = actual;
+                return count;
+            }
+
+        }
+        
+
+        //•	количество элементов в массиве «free»(свободных)
+        public int Free
+        {
+            get
+            {
+                
+                for (int i = 0; i < this.arr.GetLength(0); i++)
+                {
+               
+                    if (arr[i]!=0)
+                    {
+                        count++;
+                    }
+                }
+
+                int free = size_arr - count;
+                return free;
+            }
+        }
+        //присвоение значение по индексу или обращение по индексу
+        public int this[int index]
+        {
+            get
+            {
+                try
+                {
+                    if (index > size_arr || index < 0)
+                    {
+                        throw new OverflowException();
+                    }
+                    else
+                    {
+
+
+                        return arr[index];
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                
+                
+               
+            }
+            set
+            {
+                try
+                {
+                    if (index > size_arr || index < 0)
+                    {
+                        throw new OverflowException();
+                    }
+                    this.arr[index] = value;
+                   
+
+                }
+                catch (OverflowException ex)
+                {
+                    throw ex;
+                }
+
+            }
+
+        }
+
+        //
+        //Методы
+        //•	максимальный элемент в массиве  «Max».
+        public int  Max()
+        {
+            int max = arr[0];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                if (arr[i]>max)
+                {
+                    max = arr[i];
+                }
+            }
+
+            return max;
+        }
+        public int Min()
+        {
+            int min = arr[0];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+
+            return min;
+        }
+
+    }
+}
---
+++ L_Indeksatori_25.09.2020/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Тема: Свойства и индексаторы
+//Задание №1
+//Создать собственный класс массив «MyArray» для имитации работы одномерного массива.
+//В классе должны присутствовать:
+//Поле – содержит количество записанных значений «count»
+//Свойства
+//•	размер массива «length»
+//•	количество элементов в массиве «actual»
+//•	количество элементов в массиве «free»
+
+
+//Методы
+//•	максимальный элемент в массиве  «Max».
+//•	минимальный элемент в массиве «Min».
+
+//Конструктор с параметром (задает размерность массива)
+
+//Индексатор для обращения к элементам массива по индексу
+
+//Предусмотреть возможность возникновения исключительной ситуации с выходом за пределы массива.
+
+
+namespace L_Indeksatori_25._09._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //задаем размерность массиву внутри класса 10
+            MyArray test=new MyArray(10);
+            test[0] = 11;
+            test[3] = 22;
+            Console.WriteLine("Вывод метода free :");
+            Console.WriteLine(test.Free);
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Вывода метода actual :");
+            Console.WriteLine(test.Actual);
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Максимальное значение массива :");
+            Console.WriteLine(test.Max());
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Минимальное значение массива :");
+            Console.WriteLine(test.Min());
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Вывод отлавливания ошибки если index которому присваивают значение больше или меньше размера массива");
+            try
+            {
+                test[11] = 99;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error");
+                Console.WriteLine(ex.Message);
+              
+            }
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Вывод отлавливания ошибки если попытаться вывести згачение массива за пределами значений массива");
+            try
+            {
+                Console.WriteLine(test[11]);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error");
+                Console.WriteLine(ex.Message);
+            }
+          
+          
+        }
+    }
+}
